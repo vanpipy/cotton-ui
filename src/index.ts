115f8f7c3c6544b28fc904d055c621ccc11c9058
fanjingang <fanjingang@xjjtkjyxzrgs.com>
2022-09-23 16:44:29 +0800

    chore: update the bundles

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import LoadableButton from './LoadableButton.vue';
 import Filter from './Filter.vue';
 import { createConfirm } from './Confirm';
+import * as CONSTANS from './constant';
 
+export { CONSTANS };
 export { LoadableButton, Filter, createConfirm };
