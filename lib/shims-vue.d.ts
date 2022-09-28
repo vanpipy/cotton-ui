095d0a7dba806c5e7b4bc627006c5aa576d3630a
fanjingang <fanjingang@xjjtkjyxzrgs.com>
2022-09-28 22:13:20 +0800

    chore: build with Table

--- lib/shims-vue.d.ts
+++ lib/shims-vue.d.ts
@@ -1,4 +1,22 @@
-declare module '*' {
+declare module '*.vue' {
   import Vue from 'vue';
+  import {
+    DefaultComputed,
+    DefaultData,
+    DefaultMethods,
+    DefaultProps,
+    ThisTypedComponentOptionsWithRecordProps,
+  } from 'vue/types/options';
+
+  Vue.extend(
+    {} as ThisTypedComponentOptionsWithRecordProps<
+      Vue,
+      DefaultData<any>,
+      DefaultMethods<any>,
+      DefaultComputed<any>,
+      DefaultProps<any>
+    >
+  );
+
   export default Vue;
 }
