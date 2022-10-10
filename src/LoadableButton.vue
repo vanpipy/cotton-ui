a1d9094415bede6f0cff1acf425ee9f2bc72efc3
fanjingang <fanjingang@xjjtkjyxzrgs.com>
2022-10-10 12:58:31 +0800

    refactor(LoadableButton): migrate to vue class component

--- src/LoadableButton.vue
+++ src/LoadableButton.vue
@@ -6,78 +6,71 @@
 </template>
 
 <script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
 import { Button as ElementButton } from 'element-ui';
 import { wait } from './utils';
 
-const asyncNoop = async () => {
-  return undefined;
+const asyncNoop = async (args?: unknown) => {
+  return args;
 };
 
-export default {
-  name: 'LoadableButton',
-
-  components: { ElementButton },
-
-  props: {
-    onClick: {
-      type: Function,
-      default: asyncNoop,
-    },
-    interval: {
-      type: Number,
-      default: 300,
-    },
-    delayToTrigger: {
-      type: Number,
-      default: 0,
-    },
+@Component({
+  components: {
+    ElementButton,
   },
+})
+export default class LoadableButton extends Vue {
+  @Prop({ type: Number, default: 300 }) readonly interval!: number;
+  @Prop({ type: Number, default: 0 }) readonly delayToTrigger!: number;
+  @Prop({
+    type: Function,
+    default() {
+      return asyncNoop;
+    },
+  })
+  readonly onClick!: typeof asyncNoop;
 
-  data: () => {
-    return {
-      loading: false,
-      delay: 0,
-      timer: 0,
-    };
-  },
+  loading = false;
+  delay = 0;
+  timer: unknown;
 
-  mounted: function () {
+  mounted() {
     if (this.delayToTrigger > 0) {
       this.delay = this.delayToTrigger;
       this.countdown();
     }
-  },
+  }
 
-  unmounted: function () {
-    clearTimeout(this.timer);
-  },
+  unmounted() {
+    clearTimeout(this.timer as number);
+  }
 
-  methods: {
-    onTriggerClick: async function (...args: unknown[]) {
-      this.loading = true;
+  async onTriggerClick(...args: unknown[]) {
+    console.log();
+    this.loading = true;
 
-      await wait(this.interval);
+    await wait(this.interval);
 
-      try {
-        const onClickEvent: typeof asyncNoop = this.onClick || asyncNoop;
-        await onClickEvent(...args);
-        this.loading = false;
-      } catch (err) {
-        console.warn(err);
-        this.loading = false;
-      }
-    },
-    countdown: function () {
-      if (this.delay > 0) {
-        this.timer = window.setTimeout(() => {
-          this.delay -= 1;
-          this.countdown();
-        }, 1000);
-      } else {
-        this.onTriggerClick();
-        clearTimeout(this.timer);
-      }
-    },
-  },
-};
+    try {
+      const onClickEvent: typeof asyncNoop = this.onClick || asyncNoop;
+      await onClickEvent(...args);
+      this.loading = false;
+    } catch (err) {
+      console.warn(err);
+      this.loading = false;
+    }
+  }
+
+  countdown() {
+    if (this.delay > 0) {
+      this.timer = setTimeout(() => {
+        this.delay -= 1;
+        this.countdown();
+      }, 1000);
+    } else {
+      this.onTriggerClick();
+      clearTimeout(this.timer as number);
+    }
+  }
+}
 </script>
