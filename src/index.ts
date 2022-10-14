52c56c4bbac13f366d09082f92bab673af537edf
leroy <leroy@XJT001.localdomain>
2022-10-14 17:27:34 +0800

    refactor(bundle): restruct the exports and add release script

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,8 @@
 import LoadableButton from './LoadableButton.vue';
 import Filter from './Filter.vue';
+import Table from './Table.vue';
 import { createConfirm } from './Confirm';
 import * as CONSTANS from './constant';
 
 export { CONSTANS };
-export { LoadableButton, Filter, createConfirm };
+export { LoadableButton, Filter, Table, createConfirm };
