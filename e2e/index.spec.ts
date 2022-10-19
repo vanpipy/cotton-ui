17b0881ab347ca874296726ef5cb86f17a27dd56
vanpipy <vanpipy@gmail.com>
2022-10-19 10:43:08 +0800

    refactor(e2e): add an e2e cases to test the build result

---
+++ e2e/index.spec.ts
@@ -0,0 +1,26 @@
+import { describe, expect, it } from 'vitest';
+
+describe('E2E for the bundle result', () => {
+  it('should have a LoadableButton component', async () => {
+    const { LoadableButton } = await import('../');
+    const ESLoadableButton = await import('../lib/es/LoadableButton');
+    const CJSLoadableButton = await import('../lib/cjs/LoadableButton');
+    expect(LoadableButton).not.toBeUndefined();
+    expect(ESLoadableButton).not.toBeUndefined();
+    expect(CJSLoadableButton).not.toBeUndefined();
+  });
+
+  it('should have a Table component', async () => {
+    const { Table } = await import('../');
+    const ESTable = await import('../lib/es/Table');
+    const CJSTable = await import('../lib/cjs/Table');
+    expect(Table).not.toBeUndefined();
+    expect(ESTable).not.toBeUndefined();
+    expect(CJSTable).not.toBeUndefined();
+  });
+
+  it('should have a createConfirm component', async () => {
+    const { createConfirm } = await import('../');
+    expect(createConfirm).not.toBeUndefined();
+  });
+});
