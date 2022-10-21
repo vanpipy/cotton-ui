fe974107902d90c8a962674755da1440c4791191
vanpipy <vanpipy@gmail.com>
2022-10-21 17:15:16 +0800

    refactor(Confirm): renew Confirm component

--- src/Confirm.ts
+++ src/Confirm.ts
@@ -1,58 +1,63 @@
-import { Message, MessageBox } from 'element-ui';
+import Vue from 'vue';
+import ConfirmComponent from './Confirm.vue';
+
+const ConfirmConstructor = Vue.extend(ConfirmComponent);
 
 export type confirmProps = {
   title?: string;
   content?: string;
-  type?: 'success' | 'info' | 'warning' | 'error';
   confirmButtonText?: string;
   cancelButtonText?: string;
   closeOnClickModal?: boolean;
-};
-
-export type confirmCallback = {
-  onConfirm?: () => Promise<void>;
+  onConfirm?: () => Promise<void> | void;
+  onCancel?: () => Promise<void> | void;
 };
 
 export const createConfirm = (params: confirmProps) => {
   const {
     title = '提示',
     content = '是否继续操作',
-    type = 'warning',
     confirmButtonText = '确定',
     cancelButtonText = '取消',
-    closeOnClickModal = false,
+    onConfirm,
+    onCancel,
   } = params;
+  let instance: any;
 
-  return (callback?: confirmCallback) =>
-    new Promise<void>((resolve, reject) => {
-      const { onConfirm = async () => {} } = (callback as confirmCallback) || {};
+  const open = () => {
+    if (instance) {
+      instance.show();
+      return;
+    }
 
-      MessageBox.confirm(content, title, {
-        type,
+    instance = new ConfirmConstructor({
+      el: document.createElement('div'),
+      propsData: {
+        title,
+        content,
         confirmButtonText,
         cancelButtonText,
-        closeOnClickModal,
-        beforeClose: async (action, instance, done) => {
-          if (action === 'confirm') {
-            instance.confirmButtonLoading = true;
-
-            try {
-              await onConfirm();
-            } catch (err) {
-              Message.error(err as string);
-            }
-
-            instance.confirmButtonLoading = false;
-          } else {
-            done();
-          }
-        },
-      })
-        .then(() => {
-          resolve();
-        })
-        .catch(() => {
-          reject(Error('canceled'));
-        });
+        onConfirm,
+        onCancel,
+      },
     });
+
+    document.body.appendChild(instance.$el);
+    instance.show();
+  };
+
+  const cancel = () => {
+    try {
+      instance.hide();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  const destroy = () => {
+    document.body.removeChild(instance.$el);
+    instance = null;
+  };
+
+  return { open, cancel, destroy };
 };
