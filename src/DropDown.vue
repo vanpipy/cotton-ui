5d687ed32420d086cc24fef61f71eeb9cc006ce3
vanpipy <vanpipy@gmail.com>
2023-06-09 17:40:50 +0800

    feat: add DropDown

---
+++ src/DropDown.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="cottom-dropdown">
+    <div class="cottom-dropdown__title">
+      <slot name="title" />
+    </div>
+    <div class="cottom-dropdown__body">
+      <slot name="body" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component({})
+export default class DropDown extends Vue {}
+</script>
